e9ef2162c413de2deebd6df5b295b11b07717772
Yang Ming <13122169550@163.com>
2016-01-29 10:16:04 +0800

    连接数据库，完成userDao
    数据库:mysql
    名称:temp //没想好
    表:user //测试用

---
+++ flexOA/conf/db.js
@@ -0,0 +1,11 @@
+// conf/db.js
+// MySQL数据库联接配置
+module.exports = {
+    mysql: {
+        host: '127.0.0.1', 
+        user: 'root',
+        password: 'root',
+        database:'temp', // 数据库取啥名好呢
+        port: 3306
+    }
+};
--- flexOA/routes/users.js
+++ flexOA/routes/users.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
+var userDao = require('../dao/userDao') // userDao对象
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  //res.send('respond with a resource');
+  res.render('users');
+});
+
+/*
+  注册，调用userDao的相应函数
+ */
+router.post('/register', function(req, res, next) {
+    // [TODO]
+});
+
+/*
+  登陆，调用userDao的查询函数并验证登陆
+ */
+router.post('/login', function(req, res, next) {
+    // [TODO]    
 });
 
 module.exports = router;
