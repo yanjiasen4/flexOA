c3dd8ba6fafd6e8ed8ffdb2e7b443f8694a192c8
programmerwho <zhaiyu>
2016-02-03 15:06:54 +0800

    test

--- sailsFlexOA/api/models/user.js
+++ sailsFlexOA/api/models/user.js
@@ -1,15 +1,36 @@
-// user.js
-// Users attributes
 module.exports = {
+  connection: 'MysqlServer',
+
   attributes: {
+    userID: {
+      type: 'integer',    
+      primaryKey: true,
+      autoIncrement: true,
+      unique: true
+    },
     username: {
       type: 'string',
+      unique: true
       size: 45
     },
 
     password: {
+      type: 'string'
+    },
+
+    realname: {
       type: 'string',
+      minLength:1,
+      maxLength:20
+    },
+
+    workerID: {
+      type: 'integer',
+      defaultsTo: -1
       size: 45
     }
+
   }
+}
+\ No newline at end of file
 }
